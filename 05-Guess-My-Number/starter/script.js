9e90f9afb953825c8945b0d5132197f14517a1c9
unknown <marshallsimcox@gmail.com>
2021-04-27 23:29:13 -0600

    Add random number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,6 +9,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const number = Math.trunc(Math.random() = 20) + 1;
+document.querySelector('.number').textContent = number;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
