1de4534838faa8687bf788e0ebc76a9972f41aa5
unknown <marshallsimcox@gmail.com>
2021-05-03 22:36:16 -0600

    Modify 'No Number' and 'Correct Guess'

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -22,9 +22,18 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
+    displayMessage('⛔️ No number!');
   }else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+
   }else if (guess > secretNumber) {
     if(score > 1){
       document.querySelector('.message').textContent = 'Too High!';
