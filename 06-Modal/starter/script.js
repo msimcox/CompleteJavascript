35a39ad59d1c22d6501c3494f304f5a80d3fd6f3
unknown <marshallsimcox@gmail.com>
2021-05-04 15:35:21 -0600

    Add close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,12 +4,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  };
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-  for (let i = 0; i < btnsOpenModal.length; i++)
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//?open modal for each of the three buttons... but WHY would you want to show the same modal for each button?
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
+
+//*Close modal via "X" button
+btnCloseModal.addEventListener('click', closeModal);
+//*Close modal by clicking "outside", in the overlay
+overlay.addEventListener('click', closeModal);
