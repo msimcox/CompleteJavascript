ea9a5ab64144c8584879f6d298d8518ed12ede08
unknown <marshallsimcox@gmail.com>
2021-05-20 20:10:56 -0600

    Add loan functionality

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -212,6 +212,24 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+
+//loan functionality
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  //clear input field
+  inputLoanAmount.value = '';
+});
+
 //* End My Code *********************************
 
 
