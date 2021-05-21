3b29ddde3736925a19dbce613beb1989dccfa830
unknown <marshallsimcox@gmail.com>
2021-05-20 20:59:59 -0600

    Practice select, create & delete elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//END STARTER CODE *******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+
+// //select elements
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+//creating elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookies...<button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+//delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
