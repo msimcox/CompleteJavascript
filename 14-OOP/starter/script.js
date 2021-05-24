1236d27b563e4c9f69a290500ca279d8dacf4b1e
msimcox <msimcox84@gmail.com>
2021-05-24 13:27:05 -0600

    Practice constructor functions (OOP)

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//Section 14.205
+// Constructor Function
+const Person = function (firstName, birthYear) {
+  // Properties for the instances
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+//show that jonas is an incance of the person constructor
+console.log(jonas instanceof Person);
